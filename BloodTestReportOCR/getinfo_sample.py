20f650a25ae98cff246eea9970f6c982ccd6e5c1
xdnzxy <xdnzxy@gmail.com>
2016-11-28 13:07:01 +0800

    update getinfo_sample.py

--- BloodTestReportOCR/getinfo_sample.py
+++ BloodTestReportOCR/getinfo_sample.py
@@ -1,6 +1,6 @@
 import getinfo
 
-path = 'origin_pics/bloodtestreport3.jpg'
-num = 13
+path = 'origin_pics/bloodtestreport2.jpg'
+num = 22
 
-getinfo.getinfo(path,num)
+getinfo.getinfo(path, num, getinfo.default)
