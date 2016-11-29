5445abadfeeae36f2c3eeae7bf64263dc4259836
mengning <mengning997@163.com>
2016-11-29 10:27:36 +0800

    手工合并#40 ocr识别 by meishuguo

--- BloodTestReportOCR/ocr.py
+++ BloodTestReportOCR/ocr.py
@@ -1,21 +1,15 @@
 # -*- coding: UTF-8 -*-
-import os
+import csv
+try:
+	import Image
+except ImportError:
+	from PIL import Image
 import pytesseract
-from PIL import Image
-
-input_path='temp_pics'
-output_path='temp_nums'
-
-if not(os.path.exists(output_path)):
-    os.makedirs(output_path)
-
-# 遍历input_path下所有后缀为jpg的图片
-for i in os.listdir(input_path):
-    if os.path.splitext(i)[1] == '.jpg':
-        image = Image.open(input_path+'/'+i)
-        num = pytesseract.image_to_string(image,None,False,'-psm 7 digits') #-psm 7 digits表示识别一行数字
-        if(num!=''):
-            print i+'\t'+num
-            output = open(output_path+'/'+os.path.splitext(i)[0]+'.txt', 'w')
-            output.write(num)
-            output.close()
+result=[]
+for i in range(22):
+	image=Image.open('temp_pics/data'+str(i)+'.jpg')
+	result.append(pytesseract.image_to_string(image))
+	#result.append(pytesseract.image_to_string((image),config='-psm 7'))
+with open('data.csv', 'wb') as csvfile:
+	spamwriter = csv.writer(csvfile,dialect='excel')
+	spamwriter.writerow(result)
