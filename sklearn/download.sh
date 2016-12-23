1439cce0bccc6048543de985e74ec5bf1b93180d
xiaoxuan1123 <xxuan@mail.ustc.edu.cn>
2016-12-23 22:00:40 +0800

    update download.sh, age_predict.sh, remove data

--- sklearn/download.sh
+++ sklearn/download.sh
@@ -6,3 +6,8 @@ URL=http://home.ustc.edu.cn/~xxuan/$FILE
 echo "Downloading data.csv..."
 wget $URL -O $FILE
 echo "Done."
+
+FILE=train2.csv
+echo "Downloading train2.csv..."
+wget $URL -O $FILE
+echo "Done."
